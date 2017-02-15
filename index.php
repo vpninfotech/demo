f4fdf17b718fd0de3d4b53044fdfc0450e066534
vpninfotech <pmvpnweb@gmail.com>
2017-02-15 12:53:03 +0530

    ss
    sss

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+function compress($source, $destination, $quality) {
+
+    $info = getimagesize($source);
+
+    if ($info['mime'] == 'image/jpeg') 
+        $image = imagecreatefromjpeg($source);
+
+    elseif ($info['mime'] == 'image/gif') 
+        $image = imagecreatefromgif($source);
+
+    elseif ($info['mime'] == 'image/png') 
+        $image = imagecreatefrompng($source);
+
+    imagejpeg($image, $destination, $quality);
+
+    return $destination;
+}
+
+$source_img = 'Penguins.jpg';
+$destination_img = 'destination .jpg';
+
+$d = compress($source_img, $destination_img, 60);
+?>
